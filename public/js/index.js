e3e70daee35de3907dd2244cb1cd6b9944eaa763
GustavoSLeite <gustavo.leite@sptech.school>
2024-11-24 22:27:06 -0300

    API

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.linha1', {
+    duration: 1000,
+    distance: '200px',
+    delay:300,
+    origin: 'right'
+
+});
+
+sr.reveal('.linha2', {
+    duration: 1000,
+    distance: '200px',
+    delay:300,
+    origin: 'left'
+
+});
